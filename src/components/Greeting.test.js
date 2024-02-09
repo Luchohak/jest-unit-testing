8f15500107349124e4e48ea5dedff4c4327260d3
Luchohak <a00958934@gmail.com>
2024-02-09 14:34:22 -0600

    Testing additional functionality and also testing for elements that we dont wanna have present in the UI after a user interaction

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,7 +1,9 @@
-import Greeting from "./Greeting"
-import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 
-test('renders Hello World as a text', () => {
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
     // Arrange
     render(<Greeting />);
 
@@ -9,6 +11,42 @@ test('renders Hello World as a text', () => {
     //...nothing
 
     //Assert
-    const helloWorldElement = screen.getByText('Hello World!');
+    const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
-})
+  });
+
+  test("renders initial greeting if button is NOT clicked", () => {
+    render(<Greeting />);
+
+    const initialGreetingElement = screen.getByText("see you", {
+      exact: false,
+    });
+    expect(initialGreetingElement).toBeInTheDocument();
+  });
+
+  test("render changed if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("NOT render good to see you if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.queryByText("good to see you", { exact: false });
+    expect(outputElement).toBeNull();
+  });
+});
